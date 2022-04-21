d670d325f96a1cdc5652caec54d4f2c5637268de
twoballs-ai <Bog@482Rev>
2022-04-21 19:05:21 +0300

    настраивал login.html

--- app.py
+++ app.py
@@ -49,10 +49,10 @@ def login():
     return render_template('login.html', title='Авторизация')
 
 
-@app.route('/query-example')
-def query_example():
+@app.route('/get')
+def get():
     # if key doesn't exist, returns None
-    param_request = {'USR': 'test', 'PWD': '2100'}
+    param_request = {'USR': 'test', 'PWD': '2200'}
     response = requests.get("https://localhost/copy_1/hs/HTTP_SERVER/auth_test", params=param_request, verify=False)
     if response.status_code == 200:
         print('Success!')
@@ -63,10 +63,33 @@ def query_example():
     print("response.headers:\n{}\n\n".format(response.headers))  # Header of the request
     print("response.status_code:\n{}\n\n".format(response.status_code))  # Получить код ответа
     print("response.text:\n{}\n\n".format(response.text))  # Text Output
-    print("response.json:\n{}\n\n".format(response.json()))
+    # print("response.json:\n{}\n\n".format(response.json()))
+    print("response.encoding:\n{}\n\n".format(response.encoding))  # Узнать, какую кодировку использует Requests
+    print("response.content:\n{}\n\n".format(response.content))  # В бинарном виде
+
+    return "response.text:\n{}\n\n".format(response.text)
+
+
+@app.route('/post')
+def post(*args, **kwargs):
+    # if key doesn't exist, returns None
+    param_request = {'USR': 'test', 'PWD': '2200'}
+    response = requests.post("https://localhost/copy_1/hs/HTTP_SERVER/auth_test", params=param_request, verify=False)
+    if response.status_code == 200:
+        print('Success!')
+    elif response.status_code == 401:
+        print('Not auth.')
+    print("response:\n{}\n\n".format(response))
+    print("response.url:\n{}\n\n".format(response.url))  # Посмотреть формат URL (с параметрами)
+    print("response.headers:\n{}\n\n".format(response.headers))  # Header of the request
+    print("response.status_code:\n{}\n\n".format(response.status_code))  # Получить код ответа
+    print("response.text:\n{}\n\n".format(response.text))  # Text Output
+    # print("response.json:\n{}\n\n".format(response.json()))
     print("response.encoding:\n{}\n\n".format(response.encoding))  # Узнать, какую кодировку использует Requests
     print("response.content:\n{}\n\n".format(response.content))  # В бинарном виде
     return "response.text:\n{}\n\n".format(response.text)
+
+
 
 # пример создания ссылок
 # @app.route('/url/<int:variable>/<variable>')
@@ -79,6 +102,7 @@ def query_example():
 @app.errorhandler(404)
 def pageNotFound(error):
     return render_template('page_404.html', title='страница не найдена'), 404
+
 
 
 if __name__ == '__main__':

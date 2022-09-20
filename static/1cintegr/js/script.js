de60f6d44bd72d2e925063f3ff4e46588063a967
twoballs-ai <Bog@482Rev>
2022-09-20 23:34:04 +0300

    выход из системы

--- static/1cintegr/js/script.js
+++ static/1cintegr/js/script.js
@@ -3,88 +3,101 @@ $('tr[data-href]').on("click", function() {
 });
 
 var token = "d9d839eea6af5bf1c146189a65c734a35651b6f2";
+// Замените на свой API-ключ
 
-var type = "ADDRESS";
-var $region = $("#region");
-var $area = $("#area");
-var $city = $("#city");
-var $settlement = $("#settlement");
-var $street = $("#street");
-var $house = $("#house");
 
-function showPostalCode(suggestion) {
-  $("#postal_code").val(suggestion.data.postal_code);
+function join(arr /*, separator */) {
+  var separator = arguments.length > 1 ? arguments[1] : ", ";
+  return arr.filter(function(n){return n}).join(separator);
 }
 
-function clearPostalCode() {
-  $("#postal_code").val("");
+function geoQuality(qc_geo) {
+  var localization = {
+    "0": "точные",
+    "1": "ближайший дом",
+    "2": "улица",
+    "3": "населенный пункт",
+    "4": "город"
+  };
+  return localization[qc_geo] || qc_geo;
 }
 
-// регион
-$region.suggestions({
+function geoLink(address) {
+  return join(["<a target=\"_blank\" href=\"",
+               "https://maps.yandex.ru/?text=",
+               address.geo_lat, ",", address.geo_lon, "\">",
+               address.geo_lat, ", ", address.geo_lon, "</a>"], "");
+}
+
+function showPostalCode(address) {
+  $("#postal_code").val(address.postal_code);
+}
+
+function showRegion(address) {
+  $("#region").val(join([
+    join([address.region_type, address.region], " "),
+    join([address.area_type, address.area], " ")
+  ]));
+}
+
+function showCity(address) {
+  $("#city").val(join([
+    join([address.city_type, address.city], " "),
+    join([address.settlement_type, address.settlement], " ")
+  ]));
+}
+
+function showStreet(address) {
+  $("#street").val(
+    join([address.street_type, address.street], " ")
+  );
+}
+
+function showHouse(address) {
+  $("#house").val(join([
+    join([address.house_type, address.house], " "),
+    join([address.block_type, address.block], " ")
+  ]));
+}
+
+function showFlat(address) {
+  $("#flat").val(
+    join([address.flat_type, address.flat], " ")
+  );
+}
+
+function showGeo(address) {
+  if (address.qc_geo != "5") {
+    var geo = geoLink(address) + " (" + geoQuality(address.qc_geo) + ")";
+    $("#geo").html(geo);
+  }
+}
+
+function showSelected(suggestion) {
+  var address = suggestion.data;
+  showPostalCode(address);
+  showRegion(address);
+  showCity(address);
+  showStreet(address);
+  showHouse(address);
+  showFlat(address);
+  showGeo(address);
+}
+
+$("#address").suggestions({
   token: token,
-  type: type,
-  hint: false,
-  bounds: "region"
+  type: "ADDRESS",
+  onSelect: showSelected
 });
 
-// район
-$area.suggestions({
-  token: token,
-  type: type,
-  hint: false,
-  bounds: "area",
-  constraints: $region
-});
 
-// город и населенный пункт
-$city.suggestions({
-  token: token,
-  type: type,
-  hint: false,
-  bounds: "city",
-  constraints: $area,
-  onSelect: showPostalCode,
-  onSelectNothing: clearPostalCode
-});
-
-// geolocateCity($city);
-
-// город и населенный пункт
-$settlement.suggestions({
-  token: token,
-  type: type,
-  hint: false,
-  bounds: "settlement",
-  constraints: $city,
-  onSelect: showPostalCode,
-  onSelectNothing: clearPostalCode
-});
-
-// улица
-$street.suggestions({
-  token: token,
-  type: type,
-  hint: false,
-  bounds: "street",
-  constraints: $settlement,
-  onSelect: showPostalCode,
-  onSelectNothing: clearPostalCode
-});
-
-// дом
-$house.suggestions({
-  token: token,
-  type: type,
-  hint: false,
-  bounds: "house",
-  constraints: $street,
-  onSelect: showPostalCode,
-  onSelectNothing: clearPostalCode
-});
-
-$('.btn').click(function(){
+$('.btnTranslate1').click(function(){
+  $('#form-2 [name="name"]').val($('#form-1 [name="name"]').val());
+  $('#form-2 [name="description"]').val($('#form-1 [name="description"]').val());
   $('#form-2 [name="postal_code"]').val($('#form-1 [name="postal_code"]').val());
+  $('#form-2 [name="object_type_1"]').val($('#form-1 [name="object_type_1"]').val());
+  $('#form-2 [name="LandCategory"]').val($('#form-1 [name="LandCategory"]').val());
+  $('#form-2 [name="PurposeObject_1"]').val($('#form-1 [name="PurposeObject_1"]').val());
   $('#form-2 [name="flat"]').val($('#form-1 [name="flat"]').val());
   $('#form-2 [name="house"]').val($('#form-1 [name="house"]').val());
   $('#form-2 [name="street"]').val($('#form-1 [name="street"]').val());
@@ -92,6 +105,113 @@ $('.btn').click(function(){
   $('#form-2 [name="city"]').val($('#form-1 [name="city"]').val());
   $('#form-2 [name="area"]').val($('#form-1 [name="area"]').val());
   $('#form-2 [name="region"]').val($('#form-1 [name="region"]').val());
+  $('#form-2 [name="TypeOfPermittedUse"]').val($('#form-1 [name="TypeOfPermittedUse"]').val());
+});
+$('.btnTranslate2').click(function(){
+  $('#form-3 [name="name"]').val($('#form-1 [name="name"]').val());
+  $('#form-3 [name="description"]').val($('#form-2 [name="description"]').val());
+  $('#form-3 [name="postal_code"]').val($('#form-2 [name="postal_code"]').val());
+  $('#form-3 [name="object_type_1"]').val($('#form-2 [name="object_type_1"]').val());
+  $('#form-3 [name="LandCategory"]').val($('#form-2 [name="LandCategory"]').val());
+  $('#form-3 [name="PurposeObject_1"]').val($('#form-2 [name="PurposeObject_1"]').val());
+  $('#form-3 [name="flat"]').val($('#form-2 [name="flat"]').val());
+  $('#form-3 [name="house"]').val($('#form-2 [name="house"]').val());
+  $('#form-3 [name="street"]').val($('#form-2 [name="street"]').val());
+  $('#form-3 [name="settlement"]').val($('#form-2 [name="settlement"]').val());
+  $('#form-3 [name="city"]').val($('#form-2 [name="city"]').val());
+  $('#form-3 [name="area"]').val($('#form-2 [name="area"]').val());
+  $('#form-3 [name="region"]').val($('#form-2 [name="region"]').val());
+  // форма2
+  $('#form-3 [name="RNFI"]').val($('#form-2 [name="RNFI"]').val());
+  $('#form-3 [name="RNFI_date"]').val($('#form-2 [name="RNFI_date"]').val());
+  $('#form-3 [name="owner_nomer"]').val($('#form-2 [name="owner_nomer"]').val());
+  $('#form-3 [name="owner_date"]').val($('#form-2 [name="owner_date"]').val());
+  $('#form-3 [name="RecordNumberVEGRP"]').val($('#form-2 [name="RecordNumberVEGRP"]').val());
+  $('#form-3 [name="DateRecordsVEGRP"]').val($('#form-2 [name="DateRecordsVEGRP"]').val());
+  $('#form-3 [name="sobstvennik_on"]').val($('#form-2 [name="sobstvennik_on"]').val());
+  $('#form-3 [name="type_pravoobladatel"]').val($('#form-2 [name="type_pravoobladatel"]').val());
+  $('#form-3 [name="date_of_registration_of_another_right"]').val($('#form-2 [name="date_of_registration_of_another_right"]').val());
+  $('#form-3 [name="BalanceAccountNumber"]').val($('#form-2 [name="BalanceAccountNumber"]').val());
+  $('#form-3 [name="inventory_number').val($('#form-2 [name="inventory_number"]').val());
+  $('#form-3 [name="balance_number"]').val($('#form-2 [name="balance_number"]').val());
+  $('#form-3 [name="CadastralNumber"]').val($('#form-2 [name="CadastralNumber"]').val());
+  $('#form-3 [name="Initial_cost"]').val($('#form-2 [name="Initial_cost"]').val());
+  $('#form-3 [name="residual_value"]').val($('#form-2 [name="residual_value"]').val());
+});
+$('.btnTranslate3').click(function(){
+  $('#form-4 [name="name"]').val($('#form-3 [name="name"]').val());
+  $('#form-4 [name="description"]').val($('#form-3 [name="description"]').val());
+  $('#form-4 [name="postal_code"]').val($('#form-3 [name="postal_code"]').val());
+  $('#form-4 [name="object_type_1"]').val($('#form-3 [name="object_type_1"]').val());
+  $('#form-4 [name="LandCategory"]').val($('#form-3 [name="LandCategory"]').val());
+  $('#form-4 [name="PurposeObject_1"]').val($('#form-3 [name="PurposeObject_1"]').val());
+  $('#form-4 [name="flat"]').val($('#form-3 [name="flat"]').val());
+  $('#form-4 [name="house"]').val($('#form-3 [name="house"]').val());
+  $('#form-4 [name="street"]').val($('#form-3 [name="street"]').val());
+  $('#form-4 [name="settlement"]').val($('#form-3 [name="settlement"]').val());
+  $('#form-4 [name="city"]').val($('#form-3 [name="city"]').val());
+  $('#form-4 [name="area"]').val($('#form-3 [name="area"]').val());
+  $('#form-4 [name="region"]').val($('#form-3 [name="region"]').val());
+  // форма2
+  $('#form-4 [name="RNFI"]').val($('#form-3 [name="RNFI"]').val());
+  $('#form-4 [name="RNFI_date"]').val($('#form-3 [name="RNFI_date"]').val());
+  $('#form-4 [name="owner_nomer"]').val($('#form-3 [name="owner_nomer"]').val());
+  $('#form-4 [name="owner_date"]').val($('#form-3 [name="owner_date"]').val());
+  $('#form-4 [name="RecordNumberVEGRP"]').val($('#form-3 [name="RecordNumberVEGRP"]').val());
+  $('#form-4 [name="DateRecordsVEGRP"]').val($('#form-3 [name="DateRecordsVEGRP"]').val());
+  $('#form-4 [name="sobstvennik_on"]').val($('#form-3 [name="sobstvennik_on"]').val());
+  $('#form-4 [name="type_pravoobladatel"]').val($('#form-3 [name="type_pravoobladatel"]').val());
+  $('#form-4 [name="date_of_registration_of_another_right"]').val($('#form-3 [name="date_of_registration_of_another_right"]').val());
+  $('#form-4 [name="BalanceAccountNumber"]').val($('#form-3 [name="BalanceAccountNumber"]').val());
+  $('#form-4 [name="inventory_number').val($('#form-3 [name="inventory_number"]').val());
+  $('#form-4 [name="balance_number"]').val($('#form-3 [name="balance_number"]').val());
+  $('#form-4 [name="CadastralNumber"]').val($('#form-3 [name="CadastralNumber"]').val());
+  $('#form-4 [name="Initial_cost"]').val($('#form-3 [name="Initial_cost"]').val());
+  $('#form-4 [name="residual_value"]').val($('#form-3 [name="residual_value"]').val());
+  // 3форма
+  $('#form-4 [name="UGROKN_number"]').val($('#form-3 [name="UGROKN_number"]').val());
+});
+
+$('.btnTranslate4').click(function(){
+  $('#form-5 [name="name"]').val($('#form-4 [name="name"]').val());
+  $('#form-5 [name="description"]').val($('#form-4 [name="description"]').val());
+  $('#form-5 [name="postal_code"]').val($('#form-4 [name="postal_code"]').val());
+  $('#form-5 [name="object_type_1"]').val($('#form-4 [name="object_type_1"]').val());
+  $('#form-5 [name="LandCategory"]').val($('#form-4 [name="LandCategory"]').val());
+  $('#form-5 [name="PurposeObject_1"]').val($('#form-4 [name="PurposeObject_1"]').val());
+  $('#form-5 [name="flat"]').val($('#form-4 [name="flat"]').val());
+  $('#form-5 [name="house"]').val($('#form-4 [name="house"]').val());
+  $('#form-5 [name="street"]').val($('#form-4 [name="street"]').val());
+  $('#form-5 [name="settlement"]').val($('#form-4 [name="settlement"]').val());
+  $('#form-5 [name="city"]').val($('#form-4 [name="city"]').val());
+  $('#form-5 [name="area"]').val($('#form-4 [name="area"]').val());
+  $('#form-5 [name="region"]').val($('#form-4 [name="region"]').val());
+  // форма2
+  $('#form-5 [name="RNFI"]').val($('#form-4 [name="RNFI"]').val());
+  $('#form-5 [name="RNFI_date"]').val($('#form-4 [name="RNFI_date"]').val());
+  $('#form-5 [name="owner_nomer"]').val($('#form-4 [name="owner_nomer"]').val());
+  $('#form-5 [name="owner_date"]').val($('#form-4 [name="owner_date"]').val());
+  $('#form-5 [name="RecordNumberVEGRP"]').val($('#form-4 [name="RecordNumberVEGRP"]').val());
+  $('#form-5 [name="DateRecordsVEGRP"]').val($('#form-4 [name="DateRecordsVEGRP"]').val());
+  $('#form-5 [name="sobstvennik_on"]').val($('#form-4 [name="sobstvennik_on"]').val());
+  $('#form-5 [name="type_pravoobladatel"]').val($('#form-4 [name="type_pravoobladatel"]').val());
+  $('#form-5 [name="date_of_registration_of_another_right"]').val($('#form-4 [name="date_of_registration_of_another_right"]').val());
+  $('#form-5 [name="BalanceAccountNumber"]').val($('#form-4 [name="BalanceAccountNumber"]').val());
+  $('#form-5 [name="inventory_number').val($('#form-4 [name="inventory_number"]').val());
+  $('#form-5 [name="balance_number"]').val($('#form-4 [name="balance_number"]').val());
+  $('#form-5 [name="CadastralNumber"]').val($('#form-4 [name="CadastralNumber"]').val());
+  $('#form-5 [name="Initial_cost"]').val($('#form-4 [name="Initial_cost"]').val());
+  $('#form-5 [name="residual_value"]').val($('#form-4 [name="residual_value"]').val());
+  // 3форма
+  $('#form-5 [name="UGROKN_number"]').val($('#form-4 [name="UGROKN_number"]').val());
+  // 4 форма
+  $('#form-5 [name="encumbrance_area"]').val($('#form-4 [name="encumbrance_area"]').val());
+  $('#form-5 [name="encumbrance_cost"]').val($('#form-4 [name="encumbrance_cost"]').val());
+  $('#form-5 [name="Other_payments"]').val($('#form-4 [name="Other_payments"]').val());
+  $('#form-5 [name="start_use"]').val($('#form-4 [name="start_use"]').val());
+  $('#form-5 [name="Payment_foruse"]').val($('#form-4 [name="Payment_foruse"]').val());
+
+
 });
 
 // const myModal = document.getElementById('myModal')
